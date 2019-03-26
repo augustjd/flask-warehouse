b14ca075ffd5abd8eceec7026b6da1c2b4174f66
Joshua Augustin <augustinspring@gmail.com>
2019-03-26 12:24:08 -0700

    Fixes to README and setup.py.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     long_description=readme + '\n\n' + history,
     author="Joshua Augustin",
     author_email='augustinspring@gmail.com',
-    url='https://github.com/augustjd/flask_warehouse',
+    url='https://github.com/augustjd/flask-warehouse',
     packages=[
         'flask_warehouse',
     ],
@@ -40,9 +40,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
