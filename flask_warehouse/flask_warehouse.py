4a8c940afc5d285faab5681fadae391f565a48b3
Joshua Augustin <augustinspring@gmail.com>
2019-03-26 11:44:53 -0700

    Pulls in old code.

--- flask_warehouse/flask_warehouse.py
+++ flask_warehouse/flask_warehouse.py
@@ -1,17 +1,89 @@
+import re
+
+from flask import current_app
+
+from .service import Service, Bucket
+
+from .file import FileService
+from .s3 import S3Service
+
+
+STORAGE_BUCKET_REGEX = \
+    re.compile(r"(?P<service>(s3|file)):\/\/(?P<location>[^\/]+)\/(?P<bucket>[^\/]+)")
+STORAGE_STRING_REGEX = \
+    re.compile(r"{}\/(?P<key>.+)".format(STORAGE_BUCKET_REGEX.pattern))
+
+
 class FlaskWarehouse:
     """
     Clean abstraction over several file storage backends (S3, Alicloud, local).
     """
 
     def __init__(self, app=None):
+        self.services = {
+            's3': S3Service,
+            'file': FileService,
+        }
+
+        self.service: Service = None
+        self.default_bucket: Bucket = None
+
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
-        '''Initalizes the application with the extension.
-        :param app: The Flask application object.
-        '''
-        pass
+        self.app = app
+
+        app.config.setdefault('STORAGE_DEFAULT_SERVICE', 's3')
+        app.config.setdefault('STORAGE_DEFAULT_LOCATION', 'us-west-1')
+
+        default_service_key = self.app.config['STORAGE_DEFAULT_SERVICE']
+
+        default_location = self.app.config['STORAGE_DEFAULT_LOCATION']
+
+        self.service = self._create_service(service=default_service_key, location=default_location, app=app)
+        self.default_bucket = app.config.get('STORAGE_DEFAULT_BUCKET')
+
+    def bucket(self, name=None):
+        if self.app is None:
+            raise RuntimeError("Storage.init_app() was not called!")
+
+        if self.default_bucket is None and name is None:
+            raise Exception("'STORAGE_DEFAULT_BUCKET' is not set!")
+
+        return self.service.bucket(name or self.default_bucket)
+
+    def _create_service(self, service=None, location=None, app=None):
+        try:
+            service_constructor = self.services[service]
+        except KeyError:
+            raise Exception("No StorageService was registered named '{}'".format(service))
+
+        return service_constructor(app or current_app, default_location=location)
+
+    def _create_bucket_or_cubby(self, service=None, location=None, bucket=None, key=None, app=None):
+        service = self._create_service(service=service, location=location)
+        bucket = service.bucket(bucket)
+
+        if key is None:
+            return bucket
+
+        return bucket.cubby(key)
+
+    def __call__(self, bucket_or_key_str):
+        match = (STORAGE_STRING_REGEX.match(bucket_or_key_str)
+                 or STORAGE_BUCKET_REGEX.match(bucket_or_key_str))
+
+        if match:
+            return self._create_bucket_or_cubby(**match.groupdict())
+
+        raise Exception("Could not parse '{}' as a Bucket or Cubby str".format(bucket_or_key_str))
+
+    def __repr__(self):
+        return "<Storage service={} default_bucket={}>".format(self.service, self.default_bucket)
+
+    def __str__(self):
+        return repr(self)
 
 
 __all__ = ["FlaskWarehouse"]
---
+++ flask_warehouse/s3.py
@@ -0,0 +1,118 @@
+from tempfile import SpooledTemporaryFile
+
+from flask import Flask
+
+import boto3
+from botocore.exceptions import ClientError
+
+from .service import Bucket, Cubby, Service
+
+
+class S3Service(Service):
+    def __init__(self, app: Flask, aws_access_key_id=None, aws_secret_access_key=None, default_location=None):
+        super().__init__('s3', default_location=default_location)
+
+        self.session = boto3.Session(aws_access_key_id=aws_access_key_id,
+                                     aws_secret_access_key=aws_secret_access_key,
+                                     region_name=default_location)
+
+        self.s3 = boto3.resource('s3')
+        self.client = boto3.client('s3')
+
+        try:
+            self.client.list_buckets()
+        except Exception:
+            raise Exception("Failed to connect to S3 - ensure that AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are both set.")
+
+
+class S3Bucket(Bucket):
+    def __init__(self, service: S3Service, name: str, location: str):
+        super().__init__(service, name, location)
+
+        self._bucket = self.service.s3.Bucket(name)
+
+        try:
+            bucket_configuration = {'LocationConstraint': location}
+            self._bucket.create(CreateBucketConfiguration=bucket_configuration)
+        except ClientError:
+            pass
+
+    def cubby(self, name, content_type=None, acl='public-read'):
+        return S3Cubby(self, name, content_type=content_type, acl=acl)
+
+    def delete(self):
+        self._bucket.delete()
+
+    def list(self, prefix=None, max_keys=None, **kwargs):
+        if prefix is not None:
+            kwargs['Prefix'] = prefix
+        if max_keys is not None:
+            kwargs['MaxKeys'] = max_keys
+
+        keys = self._bucket.objects.filter(**kwargs)
+
+        return [S3Cubby(self, key.key) for key in keys]
+
+
+class S3Cubby(Cubby):
+    def __init__(self, bucket, name, content_type=None, acl='public-read', key=None):
+        super().__init__(bucket, name)
+
+        self.content_type = content_type
+
+        if key is None:
+            self._key = self.bucket._bucket.Object(name)
+        else:
+            self._key = key
+            self.name = self._key.name
+
+        self.acl = acl
+
+    def store_filelike(self, filelike):
+        copy = SpooledTemporaryFile()  # boto3 now closes the file.
+        copy.write(filelike.read())
+        copy.seek(0)
+
+        details = {'ACL': self.acl}
+
+        if self.content_type:
+            details['ContentType'] = self.content_type
+
+        self._key.upload_fileobj(copy, ExtraArgs=details)
+
+        return self.url()
+
+    def retrieve_filelike(self, filelike):
+        if filelike.closed:
+            raise Exception("File provided was already closed.")
+
+        return self._key.download_fileobj(filelike)
+
+    def url(self, expiration=Cubby.DefaultUrlExpiration):
+        service: S3Service = self.bucket.service
+        result = service.client.generate_presigned_url('get_object',
+                                                       Params={
+                                                           "Bucket": self.bucket.name,
+                                                           "Key": self.key
+                                                       },
+                                                       ExpiresIn=int(expiration.total_seconds()) if expiration else None)
+
+        if expiration is None:
+            # chop off ?AWSAccessKeyId= etc.
+            result = result.split('?')[0]
+
+        return result
+
+    def delete(self):
+        self._key.delete()
+        return not self.exists()
+
+    def filesize(self):
+        return self._key.content_length
+
+    def exists(self):
+        matches = list(self.bucket._bucket.objects.filter(Prefix=self.key))
+        return len(matches) > 0 and matches[0].key == self.key
+
+
+S3Service.__bucket_class__ = S3Bucket
