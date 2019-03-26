72dd42b79ad393056504401f822c734a74737f2b
Joshua Augustin <augustinspring@gmail.com>
2019-03-26 11:50:27 -0700

    Updates names from Storage -> Warehouse.

--- flask_warehouse/flask_warehouse.py
+++ flask_warehouse/flask_warehouse.py
@@ -8,13 +8,15 @@ from .file import FileService
 from .s3 import S3Service
 
 
-STORAGE_BUCKET_REGEX = \
+WAREHOUSE_BUCKET_REGEX = \
     re.compile(r"(?P<service>(s3|file)):\/\/(?P<location>[^\/]+)\/(?P<bucket>[^\/]+)")
-STORAGE_STRING_REGEX = \
-    re.compile(r"{}\/(?P<key>.+)".format(STORAGE_BUCKET_REGEX.pattern))
 
 
-class FlaskWarehouse:
+WAREHOUSE_CUBBY_REGEX = \
+    re.compile(r"{}\/(?P<key>.+)".format(WAREHOUSE_BUCKET_REGEX.pattern))
+
+
+class FlaskWarehouse(Service):
     """
     Clean abstraction over several file storage backends (S3, Alicloud, local).
     """
@@ -34,24 +36,24 @@ class FlaskWarehouse:
     def init_app(self, app):
         self.app = app
 
-        app.config.setdefault('STORAGE_DEFAULT_SERVICE', 's3')
-        app.config.setdefault('STORAGE_DEFAULT_LOCATION', 'us-west-1')
+        app.config.setdefault('WAREHOUSE_DEFAULT_SERVICE', 's3')
+        app.config.setdefault('WAREHOUSE_DEFAULT_LOCATION', 'us-west-1')
 
-        default_service_key = self.app.config['STORAGE_DEFAULT_SERVICE']
+        default_service_key = self.app.config['WAREHOUSE_DEFAULT_SERVICE']
 
-        default_location = self.app.config['STORAGE_DEFAULT_LOCATION']
+        self.default_location = self.app.config['WAREHOUSE_DEFAULT_LOCATION']
 
-        self.service = self._create_service(service=default_service_key, location=default_location, app=app)
-        self.default_bucket = app.config.get('STORAGE_DEFAULT_BUCKET')
+        self.service = self._create_service(service=default_service_key, location=self.default_location, app=app)
+        self.default_bucket = app.config.get('WAREHOUSE_DEFAULT_BUCKET')
 
-    def bucket(self, name=None):
+    def bucket(self, name=None, location=None):
         if self.app is None:
             raise RuntimeError("Storage.init_app() was not called!")
 
         if self.default_bucket is None and name is None:
-            raise Exception("'STORAGE_DEFAULT_BUCKET' is not set!")
+            raise Exception("'WAREHOUSE_DEFAULT_BUCKET' is not set!")
 
-        return self.service.bucket(name or self.default_bucket)
+        return self.service.bucket(name or self.default_bucket, location or self.default_location)
 
     def _create_service(self, service=None, location=None, app=None):
         try:
@@ -71,8 +73,8 @@ class FlaskWarehouse:
         return bucket.cubby(key)
 
     def __call__(self, bucket_or_key_str):
-        match = (STORAGE_STRING_REGEX.match(bucket_or_key_str)
-                 or STORAGE_BUCKET_REGEX.match(bucket_or_key_str))
+        match = (WAREHOUSE_CUBBY_REGEX.match(bucket_or_key_str)
+                 or WAREHOUSE_BUCKET_REGEX.match(bucket_or_key_str))
 
         if match:
             return self._create_bucket_or_cubby(**match.groupdict())
@@ -80,7 +82,7 @@ class FlaskWarehouse:
         raise Exception("Could not parse '{}' as a Bucket or Cubby str".format(bucket_or_key_str))
 
     def __repr__(self):
-        return "<Storage service={} default_bucket={}>".format(self.service, self.default_bucket)
+        return "<Warehouse service={} default_bucket={}>".format(self.service, self.default_bucket)
 
     def __str__(self):
         return repr(self)
