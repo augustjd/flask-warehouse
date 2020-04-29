c9d7b33a857a40388869b9646f6d77d1a172f336
Amr Awad <amro.khir@gmail.com>
2020-04-29 13:11:27 +0200

    Bump version number

--- flask_warehouse/__init__.py
+++ flask_warehouse/__init__.py
@@ -14,7 +14,7 @@
 
 __author__ = """Joshua Augustin"""
 __email__ = 'augustinspring@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 
 from .flask_warehouse import Warehouse
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='flask_warehouse',
-    version='0.1.6',
+    version='0.1.7',
     description="A clean abstraction over cloud file storage platforms like S3, Alicloud, or Heroku.",
     long_description=readme + '\n\n' + history,
     author="Joshua Augustin",
